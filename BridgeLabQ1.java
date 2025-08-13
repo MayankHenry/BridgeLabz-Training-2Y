5107d0f728de7e8c47d192588714b19de71954a4
Mayankhenry <mayank.gla_cs.aiml24@gla.ac.in>
2025-08-13 19:25:36 +0530

    Q1 Uploaded

--- BridgeLabQ1.java
+++ BridgeLabQ1.java
@@ -1,5 +1,5 @@
 public class BridgeLabQ1 {
     public static void main(String[] args) {
-        System.out.println("Welcome To BridgeLabz!");
+        System.out.println("Welcome to BridgeLabz!");
     }
 }
